71a60a1406d4b319c6dbdb79b8429d2af07a9866
Michael Chilton <michaelc962@yahoo.com>
2018-06-03 21:56:52 -0400

    Add validators, refactor routes and geocode methods

--- controllers/geocodingController.js
+++ controllers/geocodingController.js
@@ -5,71 +5,75 @@ const mapsClient = require('@google/maps').createClient({
     Promise: Promise,
 });
 
+const coordinates = async (req, res) => {
+    await _geocode(req)
+        .then((responseData) => {
+            const singleResult = _first(results);
+            const latLng = singleResult.geometry.location;
+            const address = singleResult.formatted_address;
+            console.log(`Status: ${status} - City: ${address} -`,
+                `Latitude: ${latLng.lat} -`,
+                `Longitude: ${latLng.lng}`);
 
-const geocode = async (req, res) => {
+            const responseJSON = {
+                location: address,
+                coordinates: latLng,
+            };
+
+            res.status(200).json(responseJSON);
+        })
+        .catch((err) => {
+            _handleGeocodeError(res, err);
+        });
+};
+
+const place = async (req, res) => {
+    await _reverseGeocode(req)
+        .then((responseData) => {
+            const singleResult = _first(results);
+            const address = singleResult.formatted_address;
+            console.log(`City: ${address}`);
+
+            const responseJSON = {
+                location: address,
+            };
+
+            res.status(200).json(responseJSON);
+        })
+        .catch((err) => {
+            _handleGeocodeError(res, err);
+        });
+};
+
+const _geocode = async (req) => {
     await mapsClient.geocode({
         address: req.params.address || req.query.address,
     }).asPromise()
     .then((response) => {
-        results = response.json.results;
         status = response.status;
-        responseData = {status: status, results: results};
+        results = response.json.results;
+        responseData = { status: status, results: results };
         return Promise.resolve(responseData);
     })
     .catch((err) => {
-        return new Promise((resolve, reject) => {
-            reject(err);
-        });
+        Promise.reject(err);
     });
 };
 
-const coordinates = async (req, res) => {
-    await geocode(req)
-    .then((responseData) => {
-        singleResults = _first(results);
-        latLng = singleResults.geometry.location;
-        address = singleResults.formatted_address;
-        console.log(`Status: ${status} - City: ${address} -`,
-            `Latitude: ${latLng.lat} -`,
-            `Longitude: ${latLng.lng}`);
-
-        const responseJSON = {
-            location: address,
-            coordinates: latLng,
-        };
-
-        res.send(200, responseJSON);
-    })
-    .catch((err) => {
-        _handleGeocodeError(res, err);
-    });
-};
-
-const reverseGeocode = async (req, res) => {
+const _reverseGeocode = async (req, res) => {
     // TODO logic to handle geolocation information stored on request cookies
-    const latLngStr = req.params.latLng || req.query.latLng;
-    let latLng;
-    try {
-        latLng = JSON.parse(latLngStr);
-    } catch (err) {
-        console.error(err);
-        latLng = null;
-    }
+    const latLng = req.params.latLng || req.query.latLng;
     await mapsClient.reverseGeocode({
         latlng: latLng,
     }).asPromise()
     .then((response) => {
-        const address = response.json.results[0].formatted_address;
-        console.log(`City: ${address}`);
-
-        responseJSON = {
-            location: address,
-        };
-
-        res.send(200, responseJSON);
+        status = response.status;
+        results = response.json.results;
+        responseData = { status: status, results: results };
+        return Promise.resolve(responseData);
     })
     .catch((err) => {
-        _handleGeocodeError(res, err);
+        Promise.reject(err);
     });
 };
 
@@ -78,10 +82,10 @@ const _handleGeocodeError = (res, err) => {
     const logMessage = `${err.status} - ${errorMessage}`;
     status = err.status >= 400 ? err.status : 500;
     console.error(logMessage);
-    res.send(status, errorMessage);
+    res.status(status).json({message: errorMessage});
 };
 
 module.exports = {
     coordinates: coordinates,
-    reverseGeocode: reverseGeocode,
+    place: place,
 };
