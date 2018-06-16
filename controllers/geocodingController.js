e82627adcdaca9255311d173d2a359f0e98c328e
Michael Chilton <michaelc962@yahoo.com>
2018-06-16 00:41:49 -0400

    Get async redis storage for geocode results working

--- controllers/geocodingController.js
+++ controllers/geocodingController.js
@@ -1,63 +1,41 @@
 const _first = require('lodash/first');
-// const slugify = require('slugify');
-const { rGetAsync, rSetAsync, rFetchAsync } = require('../util/redis');
+const slugify = require('slugify');
+const { rFetchAsync } = require('../util/redis');
 
 const mapsClient = require('@google/maps').createClient({
     key: process.env.GOOGLE_API_KEY,
     Promise: Promise,
 });
 
-const testRedis = async (req, res) => {
-    const method = req.params.method || req.query.method;
-
-    if (method === 'get') {
-        await rGetAsync('get_testers')
-        .then((response) => {
-            console.log(response);
-            res.status(200).send(response);
+const _coordinatesFromCache = async (address) => {
+    const key = _citySlug(address);
+    return await rFetchAsync(key, _geocode, [address])
+        .then((responseData) => {
+            return responseData;
         })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send(err);
+        .catch((err)=> {
+            return err;
         });
-    } else if (method === 'set') {
-        await rSetAsync('set_tester', 24)
-        .then((response) => {
-            console.log(response);
-            res.status(200).send(response);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send(err);
-        });
-    } else if (method === 'fetch') {
-        await rFetchAsync(req, res, 'fetch_tester', fetchCb)
-        .then((response) => {
-            console.log(response);
-            res.status(200).send(response);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send(err);
-        });
-    }
 };
 
-const fetchCb = (req, res) => {
-    return new Promise((resolve, reject) => {
-        const connVal = req.params.val || req.query.val;
-        // 2298
-        try {
-            const newVal = 1 + 3 - 5 / 2 * 6 + parseInt(connVal);
-            resolve(newVal);
-        } catch (e) {
-            reject(null);
-        }
-    });
+const _placeFromCache = async (latLng) => {
+    const key = _coordsSlug(latLng);
+    rFetchAsync(key, _reverseGeocode, [latLng])
+        .then((responseData) => {
+            return responseData;
+        })
+        .catch((err) => {
+            return err;
+        });
 };
 
 const coordinates = async (req, res) => {
-    await _geocode(req)
+    const cached = req.params.cached || req.query.cached;
+    const address = req.params.address || req.query.address;
+
+    const geocode = cached ? _coordinatesFromCache : _geocode;
+
+    await geocode(address)
         .then((responseData) => {
             const singleResult = _first(results);
             const latLng = singleResult.geometry.location;
@@ -79,7 +57,12 @@ const coordinates = async (req, res) => {
 };
 
 const place = async (req, res) => {
-    await _reverseGeocode(req)
+    const cached = req.params.cached || req.query.cached;
+    const latLng = req.params.latLng || req.query.latLng;
+
+    const reverseGeocode = cached ? _placeFromCache : _reverseGeocode;
+
+    await reverseGeocode(latLng)
         .then((responseData) => {
             const singleResult = _first(results);
             const address = singleResult.formatted_address;
@@ -96,24 +79,23 @@ const place = async (req, res) => {
         });
 };
 
-const _geocode = async (req) => {
+const _geocode = async (address) => {
     await mapsClient.geocode({
-        address: req.params.address || req.query.address,
+        address: address,
     }).asPromise()
     .then((response) => {
         status = response.status;
         results = response.json.results;
         responseData = { status: status, results: results };
-        return responseData;
+        Promise.resolve(responseData);
     })
     .catch((err) => {
         return err;
     });
 };
 
-const _reverseGeocode = async (req, res) => {
+const _reverseGeocode = async (latLng) => {
     // TODO logic to handle geolocation information stored on request cookies
-    const latLng = req.params.latLng || req.query.latLng;
     await mapsClient.reverseGeocode({
         latlng: latLng,
     }).asPromise()
@@ -136,27 +118,15 @@ const _handleGeocodeError = (res, err) => {
     res.status(status).json({message: errorMessage});
 };
 
+const _citySlug = (str) => {
+    return slugify(str.toLowerCase());
+};
+
+const _coordsSlug = (coords) => {
+    return `lat-${coords.lat}-lon-${coords.lon}`;
+};
+
 module.exports = {
     coordinates: coordinates,
     place: place,
-    testRedis: testRedis,
 };
-// ///////////////////////////////////////////////
-// const _citySlug = (str) => {
-//     return slugify(str.toLowerCase());
-// };
-
-// const _coordsSlug = (coords) => {
-//     return `lat-${coords.lat}-lon-${coords.lon}`;
-// };
-
-
-// exports.geocodeFromCache = (req, res) => {
-//     const key = `geocode_cache:${_citySlug(req.address)}`;
-
-//     // return redis.fetchAsync(key, geocode, [req, res]);
-// };
-
-// exports.reverseGeocodeFromCache = (req, res) => {
-//     const key = `reverse_geocode_cache:${_coordsSlug(req.latLng)}`;
-// };
--- util/redis.js
+++ util/redis.js
@@ -1,26 +1,30 @@
 const { promisify } = require('util');
 const redisClient = require('redis').createClient(process.env.REDIS_URL);
 
+// Results may come back (from Google) as arrays. For simplicity, these will
+// simply be stringified and store in Redis. While this is not necessarily the
+// most efficient method of doing so, it is still extremely quick for the small
+// number of results being stored
+// (likely never more than 10 objects, with maybe 5 keys each to stringify)
+
 const rGetAsync = promisify(redisClient.get).bind(redisClient);
 const rSetAsync = promisify(redisClient.set).bind(redisClient);
 
-const rFetchAsync = async (req, res, key, fetchCb = null, cbArgs = []) => {
-    // if get(key) is true
-        // return get(key)
-    // else get val from service
-    // then set(key, val)
+
+const rFetchAsync = async (key, fetchCb = null, cbArgs = []) => {
     // TODO handle TTLs
     if (fetchCb) {
         return await rGetAsync(key).then((responseVal) => {
-            // console.log(responseVal);
             if (responseVal) {
-                return responseVal;
+                const deserializedResults = _deserializeListResults(responseVal); // eslint-disable-line max-len
+                return deserializedResults;
             } else {
-                return fetchCb(req, res, ...cbArgs)
+                return fetchCb(...cbArgs)
                     .then(async (resp) => {
-                        await rSetAsync(key, resp)
+                        const serializedResults = _serializeListResults(results); // eslint-disable-line max-len
+                        await rSetAsync(key, serializedResults)
                             .then((setRes) => {
-                                return resp;
+                                return results;
                             });
                     })
                     .catch((err) => {
@@ -37,6 +41,35 @@ const rFetchAsync = async (req, res, key, fetchCb = null, cbArgs = []) => {
                 return err;
             });
     }
+};
+
+const _serializeListResults = (results) => {
+    let serializedResults;
+    try {
+        serializedResults = JSON.stringify(results);
+    } catch (e) {
+        // catch potential JSON serialziation errors, and return a blank array
+        // indicating no good results found (and preventing disruptions
+        // to future data flow), along with logging the message
+        console.error(e);
+        return [];
+    }
+    return serializedResults;
+};
+
+const _deserializeListResults = (results) => {
+    let deserializedResults;
+    try {
+        deserializedResults = JSON.parse(result);
+    } catch (e) {
+        // catch potential JSON deserialziation errors, and return a blank array
+        // indicating no good results retrieved from redis
+        // (and preventing disruptionsto future data flow),
+        // along with logging the message
+        console.error(e);
+        return [];
+    }
+    return deserializedResults;
 };
 
 module.exports = {
